1e86226826551a8779b850a2773d78f559c2d009
Robert Piringiu <robert.piringiu@emag.ro>
2024-01-25 00:00:35 +0200

    Updated styles for lualine and tmux

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -4,12 +4,12 @@
 
 return {
   "nvim-lualine/lualine.nvim",
-  -- enabled = false,
   event = "VeryLazy",
   opts = function(_, opts)
     opts.options.theme = "onedark"
     --[[add your custom lualine config here]]
-    -- opts.options.section_separators = { left = "", right = "" }
-    -- opts.options.component_separators = { left = "", right = "" }
+    opts.options.component_separators = { left = "", right = "" }
+    opts.options.section_separators = { left = "", right = "" }
+    opts.sections.lualine_z = {}
   end,
 }
