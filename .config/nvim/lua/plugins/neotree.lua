e93ac7f7c5c64447b8d7a34614906a5b8273c6ec
Robert Piringiu <robert.piringiu@emag.ro>
2024-04-16 15:22:17 +0300

    Added hidden and ignored files to neotree and removed copilot

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -1,14 +1,13 @@
-return {}
--- return {
---   "nvim-neo-tree/neo-tree.nvim",
---   enabled = false,
--- opts = {
---   filesystem = {
---     filtered_items = {
---       visible = true,
---       show_hidden_count = true,
---       hide_dotfiles = false,
---     },
---   },
--- },
--- }
+-- return {}
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+      },
+    },
+  },
+}
