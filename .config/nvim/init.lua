8364879f695994317989e61a6b6b8169bc5f7775
Robert Piringiu <robert.piringiu@emag.ro>
2024-06-09 16:51:52 +0300

    Re-enabled the netrw banner as it's causing issues

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,7 +3,6 @@ vim.g.maplocalleader = " "
 
 -- Set to true if you have a Nerd Font installed
 vim.g.have_nerd_font = true
-vim.g.netrw_banner = 0
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
