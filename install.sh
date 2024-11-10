d495b4348db25df8022626fde85f4af82cb15e08
Robert Piringiu <robert.piringiu@emag.ro>
2024-11-10 16:37:17 +0200

    Fixed phpstorm stubs for nvim

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ sudo dnf copr enable solopasha/hyprland -y
 sudo dnf copr enable atim/lazygit -y
 sudo dnf update
 sudo dnf install hyprland wofi waybar zsh hyprshot grim slurp dunst fzf ripgrep lazygit composer alacritty tmux neovim openconnect
+composer global require jetbrains/phpstorm-stubs
 cp -r .config/* ~/.config/
 cp .tmux.conf ~/
 cp .zshrc ~/
