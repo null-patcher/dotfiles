6b428ea6f12170e9a28c8d167558e35912a850c3
Robert Piringiu <robert.piringiu@emag.ro>
2025-03-31 12:35:04 +0300

    Added autocmds for persistence nvim and updated packages

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -9,3 +9,23 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end,
 })
+
+-- when vim closes, save the session
+vim.api.nvim_create_autocmd({ "VimLeavePre" }, {
+  desc = "Save session on exit",
+  callback = function()
+    require("persistence").save()
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+  desc = "Load session on start",
+  -- enable nested to prevent file type not being set issues when session is restored
+  -- https://github.com/echasnovski/mini.nvim/issues/1133#issuecomment-2282721563
+  nested = true,
+  callback = function()
+    if vim.fn.expand("%:t") == "" then
+      require("persistence").load()
+    end
+  end,
+})
